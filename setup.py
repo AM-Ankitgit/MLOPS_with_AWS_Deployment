97e96f67b2ab7b42153a6db11c25c014538bdd68
Your Name <you@example.com>
2024-08-10 23:54:37 +0530

    File Updated

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "MLOPS_with_AWS_Deployment"
+AUTHOR_USER_NAME = "AM-Ankitgit"
+SRC_REPO = "mlProject"
+AUTHOR_EMAIL = "mahalleankit007@gamil.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="Creating Open source python package for the MLOPS",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
